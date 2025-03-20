4e0084d4ae3f7ab1907d8f73426c03487234a34b
alief abdillah <aliefmabdillah09@gmail.com>
2025-03-20 23:06:12 +0700

    feat: create insntace ol map

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import piniaPersist from "pinia-plugin-persistedstate";
+import OpenLayersMap from "vue3-openlayers";
 
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(OpenLayersMap);
 app.use(pinia);
 app.mount("#app");
 
