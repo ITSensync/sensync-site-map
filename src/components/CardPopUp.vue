8b02c93d797349cbdfbcc84d8949e4243fa6ee11
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-03-21 15:53:25 +0700

    feat: create card pop up

---
+++ src/components/CardPopUp.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const { name, address, status } = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card bg-base-100 max-w-xs shadow-sm">
+    <div class="card-body">
+      <div class="flex flex-row items-center gap-2 ">
+        <div class="ml-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="size-4"
+          >
+            <path
+              d="M5.223 2.25c-.497 0-.974.198-1.325.55l-1.3 1.298A3.75 3.75 0 0 0 7.5 9.75c.627.47 1.406.75 2.25.75.844 0 1.624-.28 2.25-.75.626.47 1.406.75 2.25.75.844 0 1.623-.28 2.25-.75a3.75 3.75 0 0 0 4.902-5.652l-1.3-1.299a1.875 1.875 0 0 0-1.325-.549H5.223Z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M3 20.25v-8.755c1.42.674 3.08.673 4.5 0A5.234 5.234 0 0 0 9.75 12c.804 0 1.568-.182 2.25-.506a5.234 5.234 0 0 0 2.25.506c.804 0 1.567-.182 2.25-.506 1.42.674 3.08.675 4.5.001v8.755h.75a.75.75 0 0 1 0 1.5H2.25a.75.75 0 0 1 0-1.5H3Zm3-6a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75v3a.75.75 0 0 1-.75.75h-3a.75.75 0 0 1-.75-.75v-3Zm8.25-.75a.75.75 0 0 0-.75.75v5.25c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-5.25a.75.75 0 0 0-.75-.75h-3Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <h2 class="card-title text-xl font-bold">
+          {{ name }}
+        </h2>
+        <div
+          class="badge"
+          :class="status === 'Online' ? 'badge-success' : 'badge-error'"
+        >
+          {{ status }}
+        </div>
+      </div>
+      <div class="flex flex-row gap-2 items-start">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="size-10"
+        >
+          <path
+            fill-rule="evenodd"
+            d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <p class="text-sm">
+          {{ address }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
